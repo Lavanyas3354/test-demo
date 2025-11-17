94ff1fd41b74fd00c388cff5af58a2ae365c71b7
Lavanyas3354 <130151034+Lavanyas3354@users.noreply.github.com>
2025-11-17 03:36:11 +0000

    first

---
+++ demo.tf
@@ -0,0 +1,29 @@
+ terraform {
+  cloud {
+    organization = "lavanyacloud"
+
+    workspaces {
+      name = "test-demo-1"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = var.environment
+  }
+}
